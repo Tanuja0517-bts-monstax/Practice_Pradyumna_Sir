b81dfaffe354478f43a63b76a1fc2f6a06f69dc0
Tanuja0517-bts-monstax <tanujans0803@gmail.com>
2024-12-05 13:03:24 +0530

    added calander popupss

--- Basic_Selenium_Training/src/popups/RedBusCalenderPopup.java
+++ Basic_Selenium_Training/src/popups/RedBusCalenderPopup.java
@@ -8,11 +8,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RedBusCalenderPopup {
 	public static void main(String[] args) {
+		String month="Jan";
+		String date="4";
+		
 		WebDriver driver=new ChromeDriver();
+		boolean flag=true;
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		
 		driver.get("https://www.redbus.in/");
 		driver.findElement(By.id("onwardCal")).click();
+		while(flag) {
+			try {
+				driver.findElement(By.xpath("//span[text()='"+month+"']")).getText();
+				if((driver.findElement(By.xpath("//span[text()='"+month+"']")).getText()).equals(month))
+					flag=false;
+			}
+			catch(Exception e) {
+				driver.findElement(By.xpath("//*[name()='svg' and @id='Layer_1']")).click();
+				
+				//while(!driver.findElement(By.xpath("//span[text()='"+month+"']")).getText().equals(month)) {
+			}
+		}
+		driver.findElement(By.xpath("//div[text()='"+month+"']/parent::div/following-sibling::div/div[contains(@class,'DayTilesWrapper')]/child::span/div/span[text()='"+date+"']")).click();
 	}
 }
